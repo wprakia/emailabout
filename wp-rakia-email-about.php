e54bd5f4e635a6411e6583b2af960a6204e1c924
wprakia <47602626+wprakia@users.noreply.github.com>
2019-04-18 11:06:02 +0200

    Update wp-rakia-email-about.php
    Be sure emails sent from your WordPress are sent exactly from your WordPress ;-)

--- wp-rakia-email-about.php
+++ wp-rakia-email-about.php
@@ -7,6 +7,32 @@
  Version: 1.0
  Author URI: https://medium.com/websec
  */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+//define them in your wp-config.php or change values here into something appropriate/unique for your web site :-)
+if ( !defined("WPRAKIA_ADMIN_EMAIL_HELLO")  ) {
+    
+define("WPRAKIA_ADMIN_EMAIL_HELLO", "
+******************************************
+*                                        *
+*             hELLO Dolly                *
+*                                        *
+******************************************
+");
+
+}
+
+if ( !defined("WPRAKIA_ADMIN_EMAIL_HELLO_WSOD")  ){
+    
+define("WPRAKIA_ADMIN_EMAIL_HELLO_WSOD",  "
+************************************************************************************************
+When I receive recovery email from my website WSOD this will be first part of the email message!
+************************************************************************************************
+");
+
+}
 
 function wp_rakia_stop_admin_email_leak( $errors, $sanitized_user_login, $user_email ){
     
@@ -28,3 +54,62 @@ function wp_rakia_stop_admin_email_leak( $errors, $sanitized_user_login, $user_e
 }
 
 add_filter("registration_errors", "wp_rakia_stop_admin_email_leak", 100, 3);
+
+
+
+function wp_rakia_mark_admin_email($in = array()){
+    
+    if ( ! is_array($in) ) return array();//against coding standards :-)
+    
+    if ( isset($in['to']) && get_option('admin_email') === $in['to'] && is_email($in['to']) ){
+        
+        if ( isset($in['message']) ){
+            
+            $in['message'] = WPRAKIA_ADMIN_EMAIL_HELLO.$in['message'];
+        
+        }else{
+            
+            $in['message'] = WPRAKIA_ADMIN_EMAIL_HELLO;
+        
+        }
+        
+    }
+    return $in;
+    
+}
+
+add_filter("wp_mail", "wp_rakia_mark_admin_email", 1, 1);
+
+
+
+function wp_rakia_mark_recovery_email($in_email = array(), $in_url = ""){
+    
+    $local_to = "";
+    
+    if ( defined("RECOVERY_MODE_EMAIL") ){
+        
+        $local_to = RECOVERY_MODE_EMAIL;
+    
+    }else{
+        
+        $local_to = get_option('admin_email');
+    
+    }
+    
+    if (  is_email($local_to) && isset($in_email['to']) && $local_to === $in_email['to'] ){
+        
+        if ( isset($in_email['message']) ){
+            
+            $in_email['message'] = WPRAKIA_ADMIN_EMAIL_HELLO_WSOD.$in_email['message'];
+            
+        }else{
+            
+            $in_email['message'] = WPRAKIA_ADMIN_EMAIL_HELLO_WSOD;
+            
+        }
+    }
+    
+    return $in_email;
+}
+
+add_filter("recovery_mode_email", "wp_rakia_mark_recovery_email", 1, 2);
